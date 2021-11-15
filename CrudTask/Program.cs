dc7ec5c60bfa151cdbbee3cc6558db3339c9d8bc
Drigster <gabriel.j6e@gmail.com>
2021-11-15 22:18:08 +0200

    Initial commit

---
+++ CrudTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filename = "hardware";
+                if (args.Length >= 1)
+                {
+                    char dot = '.';
+                    filename = args[1].Split(dot)[0];
+                }
+                JsonFileSoftwareRepository repo = new JsonFileSoftwareRepository(filename);
+                ConsoleMenuApp app = new DatabaseApp(repo);
+                app.Setup();
+                app.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Fatal error: {e.Message}\n{e.StackTrace}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ CrudTask/Utils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudTask
+{
+    public class Utils
+    {
+        public static bool UserCheck(string message)
+        {
+            while (true){
+                Console.WriteLine(message + "(y/n)");
+                string inpt = Console.ReadLine();
+                if (inpt == "y")
+                {
+                    return true;
+                }
+                else if (inpt == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        public static string TruncateString(string str, int maxlength)
+        {
+            return (str.Length <= maxlength) ? str : (str.Substring(0, maxlength - 3) + "...");
+        }
+
+        public static string CheckUserInput(string message, bool stringCheck = false)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                string data = Console.ReadLine();
+                if (data == "")
+                {
+                    Console.Write("Cant be null!\n");
+                    continue;
+                }
+                else if (int.TryParse(data, out _) && stringCheck)
+                {
+                    Console.Write("Cant be number!\n");
+                    continue;
+                }
+                else
+                {
+                    return data;
+                }
+            }
+        }
+    }
+}
